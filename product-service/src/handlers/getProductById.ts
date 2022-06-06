a56ad944d8ed35aa659ae82e36db5148ef2508d6
Maksim Rynkov <maximzmei@gmail.com>
2022-06-06 21:50:13 +0400

    feat: add 404 not found response

--- product-service/src/handlers/getProductById.ts
+++ product-service/src/handlers/getProductById.ts
@@ -4,12 +4,23 @@ import productList from "../data/productList.json"
 export const getProductById = async (event: any) => {
   const productId = event.pathParameters.productId
   const foundProduct = productList.find(product => product.id === productId);
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
-    body: JSON.stringify(foundProduct,null,2),
-  };
+  if (foundProduct) {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify(foundProduct,null,2),
+    };
+  } else {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify(`Product with id: ${productId} not found`),
+    }
+  }
 };
